120ddd20069164c8ae6c006077221d0d54e4f19f
Ricky Nguyen <105029551+nguyricky@users.noreply.github.com>
2023-09-07 21:25:05 +0000

    Finished Modal and add another SVG image

--- src/components/pages/AdminViews/StudentManagement.jsx
+++ src/components/pages/AdminViews/StudentManagement.jsx
@@ -1,37 +1,79 @@
+import { useState, useRef } from "react";
 import { styles } from "../../../styles"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faUser } from "@fortawesome/free-solid-svg-icons"
+import { faPlus, faUser, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 import { notificationList } from "../../constants";
 
 const StudentManagement = () => {
+
+    const [infoModal, setinfoModal] = useState({
+        name: "",
+        email: "",
+        studentID: "",
+    });
+
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+        setinfoModal({ ...infoModal, [name]: value })
+    };
 
     return <>
     
         <h1 className={`${styles.dashHeadText} mb-5 `}> Student Management </h1>
         <div className="flex flex-row justify-between">
         <div className="indicator">
-                <button className="btn border border-white join-item bg-white text-white hover:underline hover:bg-white" onClick={()=>document.getElementById('my_modal_3').showModal()}>
-                    <FontAwesomeIcon icon={faPlus} className="text-accent"/>
-                    <FontAwesomeIcon icon={faUser} className="text-accent"/>
+                <button className="btn border border-secondary join-item bg-secondary text-white hover:underline hover:bg-secondary" onClick={()=>document.getElementById('my_modal_3').showModal()}>
+                    <FontAwesomeIcon icon={faPlus} className="text-white"/>
+                    <FontAwesomeIcon icon={faUser} className="text-white"/>
                 </button>
                 <dialog id="my_modal_3" className="modal">
                 <div className="modal-box bg-white">
                     <form method="dialog">
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-accent">✕</button>
                     </form>
-                    <h3 className="font-bold text-lg text-accent">Hello!</h3>
-                    <p className="py-4 text-accent">FORM INPROGRESS</p>
+                    <h3 className="font-bold text-[25px] text-accent mb-4">Add a Student</h3>
+                    <label className="flex flex-col text-white border-accent">
+                            <span className="font-meduim mb-2 text-[20px] text-accent font-bold">
+                                Name
+                            </span>
+                            <input
+                                type="text"
+                                placeholder="First and Last Name"
+                                name="name"
+                                onChange={onChangeHandler}
+                                className={`${styles.inputText} border border-accent mb-5`}
+                            />
+                    </label>
+                    <label className="flex flex-col text-white">
+                        <span className="font-meduim mb-2 text-[20px] text-accent font-bold">
+                            Email
+                        </span>
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            name="email"
+                            onChange={onChangeHandler}
+                            className={`${styles.inputText} border border-accent`}
+                        />
+                    </label>
+                    <div className="flex justify-center">
+                        <button className="btn bg-secondary text-white text-[20px] border border-secondary mt-6 hover:bg-secondary hover:border-secondary w-full">
+                            Submit
+                        </button>
+                    </div>
                 </div>
                 </dialog>
                 </div>
             <div className="join">
                 <div>
                     <div>
-                    <input className="input input-bordered join-item bg-white text-accent" placeholder="Search"/>
+                    <input className="input input-bordered join-item bg-white text-accent " placeholder="Search"/>
                     </div>
                 </div>
                 <div className="indicator">
-                    <button className="btn border border-secondary join-item bg-secondary text-white hover:underline hover:bg-secondary">Search</button>
+                    <button className="btn border border-secondary join-item bg-secondary text-white hover:underline hover:bg-secondary">                    
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white"/>
+                    </button>
                 </div>
             </div>
         </div>
