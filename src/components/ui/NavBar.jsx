a50333a7b9abedbf6a62a1f9fb72d7266d8a1ed1
Ricky Nguyen <105029551+nguyricky@users.noreply.github.com>
2023-09-11 01:28:49 +0000

    Editted NavBar to omit certain pages to students

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,10 +1,17 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 
 const NavBar = () => {
+
+  const location = useLocation();
+
+  const hideStudentsSectionPaths = ["/student", "/student/dashboard"];
+
+  const shouldHideStudentsSection = hideStudentsSectionPaths.includes(location.pathname);
+
   return (
     <div className="navbar bg-accent">
       <div className="navbar-start">
@@ -12,19 +19,23 @@ const NavBar = () => {
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 text-white">
-            <li><NavLink to="./student_management">Students</NavLink></li>
-            <li><NavLink to="./setting">Setting</NavLink></li>
-          </ul>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 text-white">
+              {!shouldHideStudentsSection && (
+                <li><NavLink to="./student_management">Students</NavLink></li>
+              )}
+              <li><NavLink to="./setting">Setting</NavLink></li>
+            </ul>
         </div>
 
         <div className="flex items-center flex-row">
           <NavLink className="btn btn-ghost normal-case text-xl text-white text-[24px]" to="./dashboard">Dashboard</NavLink>
 
           <div className="hidden lg:flex">
+            {!shouldHideStudentsSection && (
             <ul className="menu menu-horizontal px-1 text-white text-[18px]">
               <li className="mr-4"><NavLink to="./student_management">Students</NavLink></li>
             </ul>
+            )}
           </div>
         </div>
       </div>
