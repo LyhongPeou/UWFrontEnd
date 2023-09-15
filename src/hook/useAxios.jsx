fe6c478e9a2316d3f644180cab01d30ea7f5ce41
peoul <peoul@oregonstate.edu>
2023-09-15 15:16:59 -0700

    Added useAxios

---
+++ src/hook/useAxios.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useAxios = ({ url, method, body = null, headers = null }) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = () => {
+    axios[method](url, JSON.parse(body), JSON.parse(headers))
+      .then((res) => {
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [method, url, body, headers]);
+
+  return { response, error, loading };
+};
+
+export default useAxios;
