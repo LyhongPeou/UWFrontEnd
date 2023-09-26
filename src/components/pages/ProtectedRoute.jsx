6f4554fc739bcd48173b21ea170db4ad815ab90c
LyhongPeou <102566014+LyhongPeou@users.noreply.github.com>
2023-09-25 22:40:08 -0700

    changes to protected route

--- src/components/pages/ProtectedRoute.jsx
+++ src/components/pages/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import useAuth from "../../hook/useAuth";
-import { Route, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ roles, element }) => {
   const { isAuthenticated, roles: userRoles } = useAuth();
   const navigate = useNavigate();
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!isAuthenticated) {
       navigate("/");
     } else if (roles && !roles.some((role) => userRoles.includes(role))) {
